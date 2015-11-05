1287de2d400df5d6dcbdb6be91f0683e68ac3d30
txemagon <txema.gonz@gmail.com>
2015-11-05 10:40:23 +0100

    xoxo comentado

--- solutions/linux-c/threads/03_argumentos/xoxo.cpp
+++ solutions/linux-c/threads/03_argumentos/xoxo.cpp
@@ -8,8 +8,9 @@ struct TDatos {
     char caracter;
 };
 
-sig_atomic_t finish = 0;
+sig_atomic_t finish = 0; //sig_atomic_t se usa cuando quiero decir que no me interrumpan cuando estoy escribiendo en la variable
 
+/* Es lo que pasa cuando pulsamos ctrl+c */
 void bye(int sig_num){
     finish = 1;
     printf("\n\n\n Byeeeeee! \n\n\n");
@@ -31,14 +32,15 @@ int main(int argc, char *argv[]) {
     struct TDatos x = {'x'};
     struct TDatos o = {'o'};
 
-    struct sigaction sa;
-    sa.sa_handler = &bye;
+    struct sigaction sa; 
+    sa.sa_handler = &bye; // Se establece que la función bye gestiona el ctrl+c
     sigaction(SIGINT, &sa, NULL);
 
-
-    pthread_create(&hilo[0], NULL, &print, (void *) &x);
+    /* Creo el hilo */
+    pthread_create(&hilo[0], NULL, &print, (void *) &x); // (void *) es un molde que convierte &x en la dirección de nada
     pthread_create(&hilo[1], NULL, &print, (void *) &o);
 
+    /* Espero a que el hilo termine */
     for (int i=0; i<2; i++)
         pthread_join(hilo[i], NULL);
 
