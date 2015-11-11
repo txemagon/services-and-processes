14566df82df369102f4538f8f68d2150cdd60f8e
txemagon <txema.gonz@gmail.com>
2015-11-11 10:58:40 +0100

    Hemos contratado albañiles en la obra.

--- solutions/linux-c/semaphores/la_obra.cpp
+++ solutions/linux-c/semaphores/la_obra.cpp
@@ -4,11 +4,24 @@
 #include <pthread.h>
 
 #define NPEONES 2
+#define NALBA   40
 
 int pila = 150;
 
+pthread_mutex_t mutex_pila = PTHREAD_MUTEX_INITIALIZER;
+
 void poner(int cantidad) {
+    pthread_mutex_lock(&mutex_pila);
         pila += cantidad;
+    pthread_mutex_unlock(&mutex_pila);
+}
+
+int quitar(int cantidad) {
+    if (cantidad > pila)
+        cantidad = pila;
+    pila -= cantidad;
+
+    return cantidad;
 }
 
 void *peon(void *p) {
@@ -21,10 +34,24 @@ void *peon(void *p) {
         printf("%s: Pongo %i ladrillos-\n",
                 mi_nombre, ladrillos);
         poner(ladrillos);
+        usleep(100000 * (rand() % 3 + 1) );
     }
 }
 
-int main(int argc, char *argv[]) {
+void *albannil(void *n) {
+    int ladrillos;
+    int *mn = (int *) n;
+    int mi_nombre = *mn;
+
+    while(1) {
+        ladrillos = rand() % 2 + 1;
+        printf("%i: Quiero %i ladrillos. Cojo %i\n",
+                mi_nombre, ladrillos, quitar(ladrillos));
+        usleep(100000 * (rand() % 3 + 1) );
+    }
+}
+
+int main() {
 
     pthread_t n_peon[NPEONES];
     const char *nombre_peon[NPEONES] = {
@@ -32,10 +59,20 @@ int main(int argc, char *argv[]) {
         "Jack"
     };
 
+    pthread_t n_alba[NALBA];
+    int nombre_alba = 1;
+
     for (int i=0; i<NPEONES; i++)
         pthread_create(&n_peon[i], NULL,
                 &peon, (void *) nombre_peon[i]);
 
+    for (int i=0; i<NALBA; i++, nombre_alba++)
+        pthread_create(&n_alba[i], NULL,
+                &albannil, (void *) &nombre_alba);
+
+
+    for (int i=0; i<NALBA; i++)
+        pthread_join(n_alba[i], NULL);
 
     for (int i=0; i<NPEONES; i++)
         pthread_join(n_peon[i], NULL);
