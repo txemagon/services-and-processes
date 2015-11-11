1cebae05e44c08b3a4f954f2eb2a22efef3a9b7a
txemagon <txema.gonz@gmail.com>
2015-11-11 12:55:34 +0100

    la obra con 1000 trabajadores

--- solutions/linux-c/semaphores/la_obra.cpp
+++ solutions/linux-c/semaphores/la_obra.cpp
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-#define NPEONES 2
-#define NALBA   40
+#define NPEONES  4
+#define NALBA    1140
+#define ALBACONC 1000
+#define TOTAL 4000
 
+
+int puestos = 0;
 int pila = 150;
 
 pthread_mutex_t mutex_pila = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_edif = PTHREAD_MUTEX_INITIALIZER;
+sem_t albaforo;
 
 void poner(int cantidad) {
     pthread_mutex_lock(&mutex_pila);
@@ -16,12 +23,31 @@ void poner(int cantidad) {
     pthread_mutex_unlock(&mutex_pila);
 }
 
+int get_ladrillos() {
+    int ladrillos;
+
+
+    pthread_mutex_lock(&mutex_pila);
+    ladrillos = pila;
+    pthread_mutex_unlock(&mutex_pila);
+
+    return ladrillos;
+}
+
 int quitar(int cantidad) {
     if (cantidad > pila)
         cantidad = pila;
+
+
+    pthread_mutex_lock(&mutex_pila);
     pila -= cantidad;
+    pthread_mutex_unlock(&mutex_pila);
 
     return cantidad;
+}
+
+void cuenta_ladrillos(){
+    printf("[%i]\t", get_ladrillos());
 }
 
 void *peon(void *p) {
@@ -31,6 +57,7 @@ void *peon(void *p) {
     while(1){
         ladrillos = rand() % 10 + 10;
 
+        cuenta_ladrillos();
         printf("%s: Pongo %i ladrillos-\n",
                 mi_nombre, ladrillos);
         poner(ladrillos);
@@ -42,12 +69,21 @@ void *albannil(void *n) {
     int ladrillos;
     int *mn = (int *) n;
     int mi_nombre = *mn;
+    int cogidos = 0;
 
-    while(1) {
+    while(puestos < TOTAL) {
+        sem_wait(&albaforo);
         ladrillos = rand() % 2 + 1;
-        printf("%i: Quiero %i ladrillos. Cojo %i\n",
-                mi_nombre, ladrillos, quitar(ladrillos));
+        cuenta_ladrillos();
+        printf("%i:\tQuiero %i ladrillos.\tCojo %i\n",
+                mi_nombre, ladrillos, cogidos = quitar(ladrillos));
         usleep(100000 * (rand() % 3 + 1) );
+        sem_post(&albaforo);
+
+        pthread_mutex_lock(&mutex_edif);
+        puestos += cogidos;
+        usleep(10000 * (rand() % 3 + 1) );
+        pthread_mutex_unlock(&mutex_edif);
     }
 }
 
@@ -56,11 +92,14 @@ int main() {
     pthread_t n_peon[NPEONES];
     const char *nombre_peon[NPEONES] = {
         "Jim",
-        "Jack"
+        "Jack",
+        "Joe",
+        "James"
     };
 
     pthread_t n_alba[NALBA];
     int nombre_alba = 1;
+    sem_init(&albaforo, 0, ALBACONC);
 
     for (int i=0; i<NPEONES; i++)
         pthread_create(&n_peon[i], NULL,
@@ -76,6 +115,8 @@ int main() {
 
     for (int i=0; i<NPEONES; i++)
         pthread_join(n_peon[i], NULL);
+
+    sem_destroy(&albaforo);
 
    return EXIT_SUCCESS;
 }
