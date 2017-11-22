d6b02b8331c0a1728a4449a7004212a649adbf49
txemagon <joseluisdamdosa@gmail.com>
2017-11-22 13:23:09 +0100

    Parsing command line

--- history/201718/14_apollo/apollo_ul.cpp
+++ history/201718/14_apollo/apollo_ul.cpp
@@ -3,18 +3,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <strings.h>
+
+#define M 0x100
+#define N 0x10
 
 sig_atomic_t hijos = 0;
 
-const char *comando[3][4] = {
-    {"mail", "-dsharklasers.com", "email.addr", NULL},
-    {"mail", "-dsharklasers.com", "email.addr", NULL},
-    {"mail", "-dsharklasers.com", "email.addr", NULL}
+char const * const comando[] = {
+    "./mail -vdsharklasers.com email.addr",
+    NULL
 };
 
 void
-spawn(const (const char *lp[])
+spawn(char const * lp)
 {
+    int i = 0;
+    char *argv[N];
+    char str[M];
+    char *p_str = str;
+
     pid_t tengo_hijo = fork();
 
     if (tengo_hijo){
@@ -22,8 +31,13 @@ spawn(const (const char *lp[])
         return;
     }
 
-    execvp(lp[0], lp);
-    fprintf(stderr, "We couldn't run %s.\n", lp[0]);
+    bzero(argv, sizeof(argv));
+    strncpy(str, lp, M);
+    while ( (argv[i++] = strtok(p_str, " ")) )
+        p_str = NULL;
+
+    execvp(argv[0], argv);
+    fprintf(stderr, "We couldn't run %s.\n", argv[0]);
 }
 
 
